73320e4607d84266ff4d1b62fc8b3307326514ad
Faizy711 <faizan.s711@gmail.com>
2018-02-01 13:12:43 -0600

    adds the hide and show feature, adds a new javascript file need to do if and else statements

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-
 //for both search bars:
-
 $("#search-form").on("click", function(event) {
     event.preventDefault();
     var city = $("#city-input").val();
@@ -10,3 +8,30 @@ $("#search-formj").on("click", function(event) {
     event.preventDefault();
     var city = $("#city-input").val();
 });
+
+$(".food_container").hide();
+$(".monument_container").hide();
+$(".news_container").hide();
+$(".concert_container").hide();
+
+$("#food_icon").on("click", function(event) {
+    console.log("food");
+    $(".food_container").toggle();
+});
+
+$("#transport_icon").on("click", function(event) {
+    console.log("transportation");
+    $(".news_container").toggle();
+});
+
+$("#concerts_icon").on("click", function(event) {
+    console.log("music");
+    $(".concert_container").toggle();
+});
+
+$("#landmark_icon").on("click", function(event) {
+    console.log("landmark");
+    $(".monument_container").toggle();
+});
+
+
