6cebb7e7bdd664f72ff291407406d9ed89eb3697
kmatchu <kevinwmathew@gmail.com>
2018-02-01 13:17:50 -0600

    minor updates

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,15 @@
 
-//for both search bars:
-
 $("#search-form").on("click", function(event) {
     event.preventDefault();
     var city = $("#city-input").val();
+    console.log(city);
+    setWeatherBox(city);
 });
 
 $("#search-formj").on("click", function(event) {
     event.preventDefault();
     var city = $("#city-input").val();
+    console.log(city);
+    setWeatherBox(city);
 });
+
