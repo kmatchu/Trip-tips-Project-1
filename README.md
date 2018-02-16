0f464037e380224ee2795dcb80579fec6fc1b61a
kmatchu <kevinwmathew@gmail.com>
2018-02-16 09:40:11 -0600

    jdoc funx comments

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Trip-tips-Project-1
+CJ's Travels is a site providing you with a glimpse into the best offerings a US city has to offer. Providing you with cultural attractions, maps, trending news, dinning, weather and more! 
+
+We used a series of inter-connected tubes and very fast gophers to process your request for information and bring it to you directly from the location you are interested in visiting. We believe the series of tubes to be similar to those found in the Mario Brothers Saga. We've collected very special gophers from Prodigy.net and AOL.com after they were let go in the early 1990s. They were happy for the work. No, they have not found Princess Peach in all their travels and are only harassed by carnivorous plants. We'd also like to thank C.J. Jordan for allowing us to use her transportation method to Mexico as our inspiration for our logo. If you'd like one of your own, VW will be selling them starting in 2019. 
+
+## Getting Started
+A files that are needed to get this project up and running on your system are included in this Github Repositiory. 
+
+### Prerequisites
+No additional prerequistes are required. 
+
+### Installing
+1.  Download repository from GitHub
+2.  Open IDE
+3.  Launch Index.html
+
+## Built With
+* [BootStrap](https://getbootstrap.com) - The web framework used
+* [GoogleFonts](https://fonts.google.com/) - Fonts brought to you by the Googles
+
+## APIs Used
+* [New York Times](https://developer.nytimes.com/) - Local News Feed
+* [Yelp](https://www.yelp.com/developers) - Restaurant information
+* [Weather Underground](https://www.wunderground.com/weather/api/) - Local Weather Information
+* [Google Places](https://developers.google.com/places/) - Landmarks, Hotels, Transportation, Photos
+* [Google Maps](https://developers.google.com/maps/) - Maps 
+* [TicketMaster](https://developer.ticketmaster.com/) - Music info & ticketing
+* [Teleport](https://developers.teleport.org/) - City Photos
+
++## WireFrame
+* [Whole Site](./assets/wireframe/Project 1 Wire Frame.jpg)
+* [Events Module](./assets/wireframe/US1 - Events.jpg)
+* [Hotel Module](./assets/wireframe/US2 - Hotel.jpg)
+* [Transit Module](./assets/wireframe/US3 - Transit.jpg)
+* [Landmarks Module](./assets/wireframe/US4 - Landmarks.jpg)
+* [Restaurants Module](./assets/wireframe/US5 - Restaurants.jpg)
+* [News Module](./assets/wireframe/US6 - Trending.jpg)
+
+
+## Contributing
+If you're interested in contributing, please email us at CJ@CJsTravels.com with a proposal for a new section. 
+
+## Authors
+
+* **Kevin Mathew** - [kmatchu](https://github.com/kmatchu)
+* **Faizan Salman** - [Faizy711](https://github.com/Faizy711)
+* **Jordan Gary** - [JigSays](https://github.com/JigSays)
+
+
+## Future Development Ideas
+Ideally, we'd like to add Augmented Reality to our site to bring you directly (just like the gophers in the series of inter-connected tubes) to the city you're interested in visiting. Imagine clicking "search" and finding yourself in the center of Times Square or the top of a pyramid in Giza. 
+
+Also, we'd really like to get images for more than 265 cities worldwide and to make our site more friendly to international destinations. 
+
+We'd also like to make our site mobile-friendly. 
+
+If you're bored and would like to help us out with any of these, feel free to email CJ@CJsTravels.com! 
--- assets/javascript/renderInformationDiv.js
+++ assets/javascript/renderInformationDiv.js
@@ -1,8 +1,17 @@
+/**
+ * Using a global variable for location, reset at each new search, and the argument which will be the "genre" of the button clicked which called this, the API URL is determined. An ajax call then gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} thisGenre the value of the button that was pressed to trigger this function
+ */
 var foodRender = function(thisGenre){
     var foodURL = 'https://nu-yelp-api.herokuapp.com/api/all/' + currentLoca + '/' + thisGenre + '/9219';
                 $.ajax({
                     url: foodURL,
                     method: "GET"
+                /**
+                * Callback function for the AJAX call
+                * @param {string} response - A string which is immediately parsed into the JSON object
+                * @callback
+                */
                 }).then(function (response) {
                     var obj = JSON.parse(response);
                     var thisHTML = "";
@@ -10,11 +19,16 @@ var foodRender = function(thisGenre){
                         thisHTML += "<div class='btn-group-justified'><div class='btn-group'><button class='optionButtonFood btn btn-primary' value = " + i + ">" + obj.businesses[i].name + "</button></div></div>";
                         };
                     $(".buttonAppendHere").html(thisHTML)
+                    /**
+                    * If there is room for this new div, then it is pushed to the global display array and the render function is run again
+                    * @callback
+                    */
                     $(".optionButtonFood").on("click", function () {
                         if (displayArray.length === 4) { alert("Please clear a box") }
                         else {
                             var currentFoodVal = $(this).val();
                             var foodHTMLToAdd = "<div class='col-md-2 currentOptions thumbnail'><button class='x'>X</button><img src=" + obj.businesses[currentFoodVal].image_url + " alt="+ obj.businesses[currentFoodVal].name + " class = 'displayPhoto' /><ul><li>" + obj.businesses[currentFoodVal].name + "</li><li>" + obj.businesses[currentFoodVal].categories[0].title + "</li><li>" + obj.businesses[currentFoodVal].display_phone + "</li><li><a href=" + obj.businesses[currentFoodVal].url + ">Yelp Link</a></li></ul></div>";
+                            /** @global */
                             displayArray.push(foodHTMLToAdd);
                             render();
                         };
@@ -23,24 +37,36 @@ var foodRender = function(thisGenre){
                 });
 };
 
+/**
+ * Using the global variable for city name, reset at each new search, and the argument which will be the "genre" of the button clicked which called this, the API URL is determined. An ajax call then gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} thisGenre - the value of the button which was pressed to trigger this function
+ */
 var concertRender = function(thisGenre){
     var concertsURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + cityName + "&classificationName=" + thisGenre + "&apikey=4gaxV68thViYmaadixAGKQwTkcdccIg0";
         $.ajax({
             url: concertsURL,
             method: "GET"
+            /**
+            * Callback function for the AJAX call
+            * @param {obj} response - The JSON object
+            * @callback
+            */
             }).then(function (response) {
                 var thisHTML = "";
                 for (i = 0; i < 8; i++) {
                     thisHTML += "<div class='btn-group-justified'><div class='btn-group'><button class='optionButtonMusic btn btn-primary' value = " + i + ">" + response._embedded.events[i]._embedded.attractions[0].name + "</button></div></div>";
                     $(".buttonAppendHere").html(thisHTML)
                     };
-
-        $(".optionButtonMusic").on("click", function () {
+            /**
+            * If there is room for this new div, then it is pushed to the global display array and the render function is run again
+            * @callback
+            */
+            $(".optionButtonMusic").on("click", function () {
             if(displayArray.length === 4){ alert("Please clear a box")}
             else{
             var currentVal = $(this).val();
             var concertHTMLToAdd = "<div class='col-md-2 currentOptions thumbnail'><button class='x'>X</button><img src=" + response._embedded.events[currentVal].images[0].url + " alt="+ response._embedded.events[currentVal].name + " /><ul><li>" + response._embedded.events[currentVal].name + "</li><li>" + response._embedded.events[currentVal]._embedded.venues[0].name + ", " + response._embedded.events[currentVal].dates.start.localDate + "</li><li><a href=" + response._embedded.events[currentVal].url + ">Purchase Tickets</a></li></ul></div>";
-            // Can this be made global?
+            /** @global */
             displayArray.push(concertHTMLToAdd);
             render();   
             };
@@ -48,23 +74,36 @@ var concertRender = function(thisGenre){
     });
 };
 
+/**
+ * Using the a passed string to search, the argument which will be inserted into the API URL. An ajax call then gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} location - a string used within the google search, for transportation stations near the city name global
+ */
 var stationsRender = function(location){
     var stationsURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=transportation+station+in+" + location + "&key=AIzaSyA5S3HY_HcD1tcwYlCjWqC0HpZCkGs0_HM";
     $.ajax({
         url: stationsURL,
         method: "GET"
+        /**
+         * Callback function for the AJAX call
+         * @param {obj} response - The JSON object
+         * @callback
+         */
         }).then(function (response) {
             var thisHTML = "";
             for (i = 0; i < 8; i++) {
                 thisHTML += "<div class='btn-group-justified'><div class='btn-group'><button class='optionButtonStation btn btn-primary' value = " + i + ">" + response.results[i].name + "</button></div></div>";
                 $(".buttonAppendHere").html(thisHTML)
                 };
-    
+    /**
+    * If there is room for this new div, then it is pushed to the global display array and the render function is run again
+    * @callback
+    */
     $(".optionButtonStation").on("click", function () {
             if(displayArray.length === 4){ alert("Please clear the box")}
             else{
                 var currentVal = $(this).val();
                 var htmlToAdd = "<div class='col-md-2 currentOptions thumbnail'><button class='x'>X</button><img src=https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&sensor=false&photoreference=" + response.results[currentVal].photos[0].photo_reference + "&key=AIzaSyB-OVCwzm7e1cFAFr9n5HEmLTPySWZSoto alt='picture' /><ul><li>" + response.results[currentVal].name + "</li><li>" +response.results[currentVal].formatted_address +"</li><li> <a href= https://www.google.com/maps/search/" + response.results[currentVal].name + " class='btn btn-primary' role='button' target='_blank'>Website</a></li></ul></div>"
+                /** @global */
                 displayArray.push(htmlToAdd);
                 render();   
                 };
@@ -72,11 +111,20 @@ var stationsRender = function(location){
         });
 };
 
+/**
+ * Using the passed variable for location, which will be the 'genre' of the clicked button which will be a string added onto the city name global, reset at each new search, the API URL is determined and an ajax call then gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} location the value of the button that was pressed to trigger this function, briefly called the 'genre'
+ */
 var hotelRender = function(location){
     var hotelURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=hotel+" + location + "&key=AIzaSyA5S3HY_HcD1tcwYlCjWqC0HpZCkGs0_HM";
     $.ajax({
         url: hotelURL,
         method: "GET"
+        /**
+         * Callback function for the AJAX call
+         * @param {obj} response - The JSON object
+         * @callback
+         */
         }).then(function (response) {
             var thisHTML = "";
             for (i = 0; i < 8; i++) {
@@ -84,11 +132,16 @@ var hotelRender = function(location){
                 $(".buttonAppendHere").html(thisHTML);
                 };
     
+    /**
+    * If there is room for this new div, then it is pushed to the global display array and the render function is run again
+    * @callback
+    */
     $(".optionButtonHotel").on("click", function () {
             if(displayArray.length === 4){ alert("Please clear the box")}
             else{
                 var currentVal = $(this).val();
                 var htmlToAdd = "<div class='col-md-2 currentOptions thumbnail'><button class='x'>X</button><img src=https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&sensor=false&photoreference=" + response.results[currentVal].photos[0].photo_reference + "&key=AIzaSyB-OVCwzm7e1cFAFr9n5HEmLTPySWZSoto alt='picture' /><ul><li>" + response.results[currentVal].name + "</li><li>" +response.results[currentVal].formatted_address +"</li><li> <a href= https://www.google.com/maps/search/" + response.results[currentVal].name + " class='btn btn-primary' role='button' target='_blank'>Website</a></li></ul></div>"
+                /** @global */
                 displayArray.push(htmlToAdd);
                 render();   
             };
@@ -96,56 +149,86 @@ var hotelRender = function(location){
     });
 };
 
+/**
+ * Using the passed variable for location, which will be the 'genre' of the clicked button which will be the city name global, reset at each new search, the URL is determined and not an ajax call, just a standard link, gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} location the value of the button that was pressed to trigger this function, briefly called the 'genre'
+ */
 var mapRender = function(location){
     var mapURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + location + "&zoom=13&size=600x300&maptype=roadmap&key=AIzaSyA5S3HY_HcD1tcwYlCjWqC0HpZCkGs0_HM";
     if(displayArray.length === 1){ alert("Please clear the box")}
     else{
     var htmlToAdd = "<div class='col-md-6 currentOptions'><button class='x'>X</button><img src=" + mapURL + " alt='citymap' height = '100%' /></div>";
+    /** @global */
     displayArray.push(htmlToAdd);
     render();   };
 };
 
+/**
+ * Using the passed variable for location, which will be the 'genre' of the clicked button which will be the city name global, reset at each new search, the API URL is determined and an ajax call then gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} location the value of the button that was pressed to trigger this function, briefly called the 'genre'
+ */
 var newsRender = function(location){
     var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=9bea106de39c43339a278e2b55fad5ef&q=" + location;
     $.ajax({
         url: nytURL,
         method: "GET"
+        /**
+         * Callback function for the AJAX call
+         * @param {obj} response - The JSON object
+         * @callback
+         */
         }).then(function (response) {
         var thisHTML = "";
         for (i = 0; i < 8; i++) {
             thisHTML += "<div class='btn-group-justified'><div class='btn-group'><button class='optionButtonNews btn btn-primary' value = " + i + ">" + response.response.docs[i].headline.main + "</button></div></div>";
             $(".buttonAppendHere").html(thisHTML);
         };
-
+                /**
+                * If there is room for this new div, then it is pushed to the global display array and the render function is run again
+                * @callback
+                */
                 $(".optionButtonNews").on("click", function () {
                 if(displayArray.length === 2){ alert("Please clear the box")}
                 else{
                 var currentVal = $(this).val();
                 var htmlToAdd = "<div class='col-md-3 currentOptions thumbnail'><button class='x'>X</button><img src=https://static01.nyt.com/" + response.response.docs[currentVal].multimedia[0].url + " /><ul><li><h3>" + response.response.docs[currentVal].headline.main + "</h3></li><li font-size= 11px>" + response.response.docs[currentVal].snippet + "</li><li><a href =" + response.response.docs[currentVal].web_url + " target = '_blank'>Read More</a></ul></div>"; 
+                /** @global */
                 displayArray.push(htmlToAdd);
                 render();
                 }
             });
         });
     };
-
+/**
+ * Using passed-in long strings which display the google results we want, we build the correct API url and an ajax call then gets any information needed to complete the framework of the information div, and appends and displays it all on the page.
+ * @param {string} types - prebuilt long strings, passed by being the value of the button which was click
+ */
     var attractionRender = function(types){
     var attractURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + currentLoca2 + "&radius=10000&rankby=prominence&types=" + types + "&key=AIzaSyB-OVCwzm7e1cFAFr9n5HEmLTPySWZSoto";
     $.ajax({
         url: attractURL,
         method: "GET"
+        /**
+         * Callback function for the AJAX call
+         * @param {obj} response - The JSON object
+         * @callback
+         */
     }).then(function (response) {
         var thisHTML = "";
         for (i = 0; i < 8; i++) {
             thisHTML += "<div class='btn-group-justified'><div class='btn-group'><button class='optionButtonAttract btn btn-primary' value = " + i + ">" + response.results[i].name + "</button></div></div>";
             $(".buttonAppendHere").html(thisHTML);
         };
-
+        /**
+         * If there is room for this new information, then it is pushed to the global display array and the render function is run again
+         * @callback
+         */
         $(".optionButtonAttract").on("click", function () {
             if(displayArray.length === 4){ alert("Please clear the box")}
                 else{
             var currentVal = $(this).val();
             var htmlToAdd = "<div class='col-md-2 currentOptions thumbnail'><button class='x'>X</button><img src=https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&sensor=false&photoreference=" + response.results[currentVal].photos[0].photo_reference + "&key=AIzaSyB-OVCwzm7e1cFAFr9n5HEmLTPySWZSoto alt='picture' /><ul><li>" + response.results[currentVal].name + "</li><li>" + response.results[currentVal].vicinity + "</li><li><a target=_blank href=https://www.google.com/maps/place/" + response.results[currentVal].vicinity.replace(/ /g, "+") + ">Google Map Location</a></li></ul>";
+                /** @global */
                 displayArray.push(htmlToAdd);
                 render();
         };
@@ -153,13 +236,18 @@ var newsRender = function(location){
 });
 };
 
-
+/** 
+ * This clears the display array and re-renders the display, clearing the boxes when you switch 'genres'
+*/
 var reset = function(){
     displayArray = [];
     render();
     $(".buttonAppendHere").html("");
 }
 
+/** 
+ * This uses the global display array to make the display, after deleting the current display. The array elements are already strings of html to be appended as divs. The x button attached to each div is assigned a value corresponding to its place in the array.
+*/
 var render = function(){
     $(".currentOptions").remove();
     for(var i=0;i<displayArray.length;i++){
@@ -171,6 +259,10 @@ var render = function(){
     
 };
 
+/**
+ * Deletes an element from the display array at the index of the value of the x button clicked. The display is then re-rendered with the new, smaller display array.
+ * @callback
+ */
 $(document).on("click",".x", function(){
     current = $(this).val();
     displayArray.splice(current,1);
